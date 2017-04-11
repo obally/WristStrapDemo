3af55a821e95e8c19af2f8438e0f78143434032b
liujiao <2603729194@qq.com>
2017-04-11 09:50:35 +0800

    Initial Commit

---
+++ WristStrapDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WristStrapDemo
+//
+//  Created by obally on 17/4/11.
+//  Copyright © 2017年 obally. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WristStrapDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WristStrapDemo
+//
+//  Created by obally on 17/4/11.
+//  Copyright © 2017年 obally. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
