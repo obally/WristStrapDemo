b466de9e6224ead90c1b8be2fa8299cff5b5fdf6
liujiao <2603729194@qq.com>
2017-04-19 11:52:30 +0800

    mgjrouter

--- WristStrapDemo/WristStrapDemo/WristStrapHome/Common/WristStrapCommon.h
+++ WristStrapDemo/WristStrapDemo/WristStrapHome/Common/WristStrapCommon.h
@@ -11,7 +11,7 @@
 
 #import "WCDSharkeyFunction.h"
 #import "OBBase.h"
-#import "MGJRouter.h"
+#import <MGJRouter/MGJRouter.h>
 #define RGBAlphaColor(a,b,c,ap) [UIColor colorWithRed:((float)a)/255.0 green:((float)b)/255.0 blue:((float)c)/255.0 alpha:ap]
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0x00FF00) >>  8))/255.0 blue:((float)((rgbValue & 0x0000FF) >>  0))/255.0 alpha:1.0]
 #define RGBColor(a,b,c) [UIColor colorWithRed:((float)a)/255.0 green:((float)b)/255.0 blue:((float)c)/255.0 alpha:1.0]
